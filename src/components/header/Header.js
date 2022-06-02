aacbaaf703f3cebb11c255b542e320c6c47197f6
Дмитрий Патока <dmitrypatoka@ya.ru>
2022-06-03 04:55:40 +0600

    Header: fixed jumping header end for non-auth

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -21,15 +21,16 @@ const Header = ({user}) => {
                 </Link>
             </div>
 
-            {loggedIn &&
-                <div className={'navbar-start'}>
+
+            <div className={'navbar-start'}>
+                {loggedIn &&
                     <div className={'navbar-tabs'}>
                         <Tab location={'/users'} pageName={'users'}><FormattedMessage id="users" /></Tab>
                         <Tab location={'/sections'} pageName={'sections'}><FormattedMessage id="sections" /></Tab>
                         <Tab location={'/notes'} pageName={'search'}><FormattedMessage id="search" /></Tab>
                     </div>
-                </div>
-            }
+                }
+            </div>
 
             <div className={'navbar-end'}>
                 <div className={'navbar-group'}>
